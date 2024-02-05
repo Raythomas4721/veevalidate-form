950a379d79cf8c7a5ea39fb29b6597086a4b04d3
ray <ray123321123321@gmail.com>
2024-02-05 15:20:52 -0800

    component error

---
+++ productModal.js
@@ -0,0 +1,68 @@
+export default {
+  template: `<div
+    class="modal fade"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    ref="productModal"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>{{ tempProduct.title }}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close btn-close-white"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-6">
+              <div
+                :style="{
+                    height: '100%',
+                    backgroundImage: 'url(' + tempProduct.imageUrl + ')',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }"
+              ></div>
+            </div>
+            <div class="col-sm-6">
+              <span class="badge bg-primary rounded-pill"
+                >{{ tempProduct.category }}</span
+              >
+              <p>商品描述：{{ tempProduct.description}}</p>
+              <p>商品內容：{{ tempProduct.content }}</p>
+              <del class="h6">原價 {{tempProduct.origin_price }} 元</del>
+              <div class="h5">現在只要 {{tempProduct.price }} 元</div>
+              <div>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    min="1"
+                    v-model.number="setQty"
+                  />
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="addToCart(tempProduct.id,setQty)"
+                  >
+                    加入購物車
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- col-sm-6 end -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`,
+  props: ["tempProduct", "setQty", "addToCart"]
+};
